5e1694f7ba48eb4764f5471bee9cf714e026aad1
Adrián Litardo <lalitard@espol.edu.ec>
2024-01-14 13:38:58 -0500

    modyfing files

--- swagger.js
+++ swagger.js
@@ -1,7 +1,6 @@
 const swaggerAutogen = require('swagger-autogen')()
 
 const outputFile = './swagger_output.json'
-const endpointsFiles = ['./routes/users.js', './routes/index.js', './models/index.js', './models/init-models.js', './models/roles.js',
-'./models/users_roles.js','./models/users.js' ]
+const endpointsFiles = ['./routes/users.js', './routes/index.js']
 
 swaggerAutogen(outputFile, endpointsFiles)
